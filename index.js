acac561be7638de8d64f09cd4f873a1ba000ddb8
ParkerPiter <gabriel.gaboz123@gmail.com>
2023-11-24 00:14:34 -0400

    original vercerl.json

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const app = require("./src/app")
+const express = require('express');
 const cors = require('cors');
-const app = express();
+const app = require('./src/app'); // Aquí es donde importas app.js
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
     origin: ['https://spontaneous-salamander-a129ea.netlify.app', 'http://127.0.0.1:5173']
-  }));
+}));
 
-
-app.listen(port, ()=> console.log(`El servidor esta funcionando y escucha en el puerto ${port}` ))
+app.listen(port, ()=> console.log(`El servidor esta funcionando y escucha en el puerto ${port}`));
